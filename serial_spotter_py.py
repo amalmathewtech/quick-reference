47d663fe1ab3a2dce4edd06e2c2468078bb0747f
AMAL MATHEW <amalmetck@gmail.com>
2022-10-29 00:38:49 +0530

    serial_spotte_py.py file added
    Python code  to list all available serial ports.

---
+++ serial_spotter_py.py
@@ -0,0 +1,6 @@
+# Listing available com ports with Python
+import serial.tools.list_ports
+ports = serial.tools.list_ports.comports()
+
+for port, desc, hwid in sorted(ports):
+        print("{}: {} [{}]".format(port, desc, hwid))
